5526134a728e910bfb02dc644ac849a6ddf32f43
agnivabasak <agnivabasak1@gmail.com>
2025-07-20 14:10:24 +0530

    Added unit test cases for sublist

---
+++ tests/test_sublist.cpp
@@ -0,0 +1,110 @@
+#include <gtest/gtest.h>
+#include "../include/nats/sublist.hpp"
+#include "../include/nats/subscription.hpp"
+
+using namespace nats;
+
+TEST(NatsSublistTest, AddAndGetSubscription) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> subject = {"foo", "bar"};
+
+    sublist.addSubscription(sub, subject);
+
+    auto result = sublist.getSubscriptionsForTopic(subject);
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], sub);
+}
+
+TEST(NatsSublistTest, RemoveSubscription) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> subject = {"foo", "bar"};
+
+    sublist.addSubscription(sub, subject);
+    sublist.removeSubscription(sub, subject);
+
+    auto result = sublist.getSubscriptionsForTopic(subject);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(NatsSublistTest, WildcardSingleLevel) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> wildcard = {"foo", "*"};
+    std::vector<std::string> match = {"foo", "bar"};
+
+    sublist.addSubscription(sub, wildcard);
+
+    auto result = sublist.getSubscriptionsForTopic(match);
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], sub);
+}
+
+TEST(NatsSublistTest, WildcardMultiLevel_1) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> wildcard = {"foo", ">"};
+    std::vector<std::string> match = {"foo", "bar", "baz"};
+
+    sublist.addSubscription(sub, wildcard);
+
+    auto result = sublist.getSubscriptionsForTopic(match);
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], sub);
+}
+
+TEST(NatsSublistTest, WildcardMultiLevel_2) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> wildcard = {"foo", "*", "bar", ">"};
+    std::vector<std::string> match = {"foo", "test", "bar", "baz", "somethingElse"};
+
+    sublist.addSubscription(sub, wildcard);
+
+    auto result = sublist.getSubscriptionsForTopic(match);
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], sub);
+}
+
+TEST(NatsSublistTest, NoMatch) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> subject = {"foo", "bar"};
+
+    sublist.addSubscription(sub, subject);
+
+    std::vector<std::string> nonmatch = {"foo", "baz"};
+    auto result = sublist.getSubscriptionsForTopic(nonmatch);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(NatsSublistTest, DuplicateAdd) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> subject = {"foo", "bar"};
+
+    sublist.addSubscription(sub, subject);
+    sublist.addSubscription(sub, subject);
+
+    auto result = sublist.getSubscriptionsForTopic(subject);
+    ASSERT_EQ(result.size(), 1); // Should not duplicate
+}
+
+TEST(NatsSublistTest, RemoveNonExistent) {
+    NatsSublist sublist;
+    NatsSubscription sub{1, 100};
+    std::vector<std::string> subject = {"foo", "bar"};
+
+    // Remove before add should not crash
+    sublist.removeSubscription(sub, subject);
+
+    // Add and remove different subscription
+    sublist.addSubscription(sub, subject);
+    NatsSubscription sub2{2, 200};
+    sublist.removeSubscription(sub2, subject);
+
+    auto result = sublist.getSubscriptionsForTopic(subject);
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], sub);
+}
